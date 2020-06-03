822136e96803ce6c500ef9c6787f9b3780aeadff
YosephAhmed <yosephahmed@aol.com>
2020-06-03 10:09:30 -0400

    Personalized App.js and ContactCard.js

--- src/App.js
+++ src/App.js
@@ -3,30 +3,20 @@ import ContactCard from "./ContactCard";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <>
-        <ContactCard
-          name="John"
-          mobile="2125555555"
-          work="8002222222"
-          email="hello@example.com"
-        />
-        <ContactCard
-          name="Jill"
-          mobile="7185555555"
-          work="7182222222"
-          email="jill@example.org"
-        />
-        <ContactCard
-          name="Mike"
-          mobile="3475555555"
-          work="5552222222"
-          email="mike@example.com"
-        />
-      </>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <ContactCard
+                    name="Bob"
+                    email="email@email.com"
+                />
+                <ContactCard
+                    name="Rod"
+                    email="email@email.com"
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -7,8 +7,6 @@ class ContactCard extends Component {
       <div>
         <h1>{this.props.name}</h1>
         <ul>
-          <li>mobile: {this.props.mobile}</li>
-          <li>work: {this.props.work}</li>
           <li>email: {this.props.email}</li>
         </ul>
       </div>
@@ -18,17 +16,7 @@ class ContactCard extends Component {
 
 ContactCard.propTypes = {
   name: PropTypes.string.isRequired,
-  mobile: PropTypes.string.isRequired,
-  work: PropTypes.string,
   email: PropTypes.string.isRequired,
 };
 
 export default ContactCard;
-
-/**
- * Exercise: Contact Cards
-Props and PropTypes
-Define a ContactCard component that takes in a contact name, mobile number, work phone number and email as properties and displays the results in a visually appealing way.
-The name, mobile number and email are required, but the work phone number is optional.
-Create a page that display at least 3 different contact cards with different information.
- */
